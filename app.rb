e9d3fa7487a1c0bfd27ab34923e5cf722828374d
Alyssa Brown <alyssa.brown@live.com>
2019-04-18 17:26:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,6 +16,15 @@ class App < Sinatra::Base
     @c = params["c"]
     erb :team 
   end 
-
+post "/team" do
+  @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @pf = params["pf"]
+    @sf = params["sf"]
+    @c = params["c"]
+    erb :team
+  end	  end
 
 end
